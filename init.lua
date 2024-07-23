e70fbe81ecd2fbe93ba496f926bfe029c965cb07
9manhasan <workwithnauman@gmail.com>
2024-07-24 00:37:18 +0530

    added tree sitter for c cpp and python

--- init.lua
+++ init.lua
@@ -189,6 +189,7 @@ require('lazy').setup {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup {
+        ensure_installed = { "c", "cpp", "python", "lua"},
         highlight = {
           enable = true,
         },
