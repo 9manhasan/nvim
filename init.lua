e89cf5f76418367046af3e3e59eb6471b07cadc4
9manhasan <workwithnauman@gmail.com>
2024-07-24 00:44:56 +0530

    added lsp with keymaps

--- init.lua
+++ init.lua
@@ -299,3 +299,12 @@ vim.opt.undofile = true
 -- Key mappings for undo and redo
 vim.api.nvim_set_keymap('n', 'u', ':undo<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-r>', ':redo<CR>', { noremap = true })
+
+-- Set up key mappings for LSP commands
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
+
