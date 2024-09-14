fc91ebb47bd94764797600d63e85d955f8fc9e54
nauman <144601779+9manhasan@users.noreply.github.com>
2024-09-15 03:36:28 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,135 +1,12 @@
--- Set leader key
-vim.g.mapleader = ' '
-
--- Install lazy.nvim if not present
+-- Auto-install Lazy.nvim if not installed
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--branch=stable',
-    'https://github.com/folke/lazy.nvim.git',
-    lazypath,
+    'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', lazypath
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Setup lazy.nvim with plugins
-require('lazy').setup({
-  -- Plugin list
-  {
-    'kdheepak/lazygit.nvim',
-    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = { { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" } },
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-  },
-  {
-    'ThePrimeagen/vim-be-good',
-  },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
-    },
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('telescope').setup({
-        defaults = {},
-      })
-      require('telescope').load_extension('fzf')
-
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-          winblend = 10,
-          previewer = false,
-        }))
-      end, { desc = '[/] Fuzzily search in current buffer' })
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep({
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        })
-      end, { desc = '[S]earch [/] in Open Files' })
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files({ cwd = vim.fn.stdpath('config') })
-      end, { desc = '[S]earch [N]eovim files' })
-    end,
-  },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-    cond = function()
-      return vim.fn.executable('make') == 1
-    end,
-  },
-  {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function()
-      local alpha = require('alpha')
-      local dashboard = require('alpha.themes.dashboard')
-      dashboard.section.header.val = {
-        [[  _ __   ___  _____   __ __ __ __   ]],
-        [[ | '_ \ / _ \/ _ \ \ / / | '_ ` _ \ ]],
-        [[ | | | |  __/ (_) \ V /| | | | | | |]],
-        [[ |_| |_|\___|\___/ \_/ |_|_| |_| |_|]],
-      }
-      dashboard.section.buttons.val = {
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-        dashboard.button("r", "  Recently opened files", ":Telescope oldfiles<CR>"),
-        dashboard.button("s", "  Settings", ":e $MYVIMRC<CR>"),
-        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-      }
-      alpha.setup(dashboard.opts)
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'c', 'cpp', 'python', 'lua' },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-  {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
-    keys = {
-      { '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<C-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    },
-  },
-})
 
 -- Basic settings
 vim.opt.clipboard = 'unnamedplus'
@@ -150,42 +27,145 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
+vim.g.have_nerd_font = false
+vim.opt.undofile = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- TIP: Disable arrow keys in normal mode
+ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+ vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Key mappings
+local opts = { noremap = true, silent = true }
+
+-- Lazy.nvim Plugin Manager Setup
+require('lazy').setup({
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require('lualine').setup {
+                options = { theme = 'tokyonight' }
+            }
+        end,
+    },
+
+    -- Git integration
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+        end,
+    },
+
+    -- Color scheme
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+
+    -- LazyGit integration
+    "kdheepak/lazygit.nvim",
+
+    -- Tmux navigation
+    "christoomey/vim-tmux-navigator",
+
+    -- NvimTree for file explorer
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require('nvim-tree').setup()
+        end,
+    },
+
+    -- Telescope for fuzzy finder
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+
+    -- Telescope Projects for project management
+    {
+        'nvim-telescope/telescope-project.nvim',
+        config = function()
+            require('telescope').load_extension('project')
+        end,
+    },
+
+    -- Treesitter for Syntax Highlighting
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+
+    -- Catppuccin theme
+    { "catppuccin/nvim", name = "catppuccin" },
+})
+
+-- Plugin-specific keymaps
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>sf', ':Telescope find_files<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>sg', ':Telescope live_grep<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>sb', ':Telescope buffers<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>p', ':Telescope projects<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>lg', ':LazyGit<CR>', opts)
+-- Tmux navigation
+vim.g.tmux_navigator_no_mappings = 1
+vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', opts)
+vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', opts)
+
+-- Tab management
+vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>to', ':tabonly<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>tm', ':tabmove<CR>', opts)
+
+-- Split window navigation
+vim.api.nvim_set_keymap('n', '<leader>sv', ':vsp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>sh', ':sp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>sc', '<C-w>c', opts)
+vim.api.nvim_set_keymap('n', '<leader>so', '<C-w>o', opts)
+vim.api.nvim_set_keymap('n', '<leader>sr', '<C-w>r', opts)
+
+-- Terminal
+vim.api.nvim_set_keymap('n', '<leader>t', ':term<CR>', opts)
 
 -- Vertical movements
-vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
-vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true })
-vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', opts)
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', opts)
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', opts)
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', opts)
 
 -- Highlight yanked text
-vim.api.nvim_set_hl(0, 'YankHighlight', { ctermbg = 'yellow', bg = 'yellow' })
-vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  pattern = '*',
+  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = 'YankHighlight', timeout = 200 })
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })
   end,
 })
 
--- Save file shortcuts
-vim.api.nvim_set_keymap('n', '<leader>wq', ':w<CR>', { noremap = true })
-
---will use noetree
-vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree reveal<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>c', ':Neotree close<CR>', { noremap = true, silent = true })
-
--- Tab management
-vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>to', ':tabonly<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>tm', ':tabmove<CR>', { noremap = true })
-
--- Split window navigation
-vim.api.nvim_set_keymap('n', '<leader>sv', ':vsp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>sh', ':sp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>sc', '<C-w>c', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>so', '<C-w>o', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>sr', '<C-w>r', { noremap = true })
-vim.cmd('colorscheme catppuccin')
-vim.api.nvim_set_keymap('n', '<leader>t', ':term<CR>', { noremap = true, silent = true })
+-- Set colorscheme
+-
